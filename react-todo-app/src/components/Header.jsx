49c94cd0ec68946d9d7ea61b2cd96b183606cbbc
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-05-09 18:27:05 +0430

    Create Header.jsx

---
+++ react-todo-app/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import styles from "@/styles/Header.module.css";
+
+const Header = () => {
+  return (
+    <header style={{
+      padding: '20px 0',
+      lineHeight: '1.5em',
+      color: '#aeadad',
+      textAlign: 'center',
+    }} className={styles.header}>
+      <h1>todos</h1>
+      <p>Items will persist in the browser local storage</p>
+    </header>
+  );
+};
+export default Header;
